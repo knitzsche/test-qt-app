cefc33788f53d691ab187ceb7b469fbdd57b75c2
Kyle Nitzsche <kyle.nitzsche@canonical.com>
2021-09-08 18:17:55 -0400

    Show a dancing gif

--- launchers/daemon.sh
+++ launchers/daemon.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-exec $SNAP/bin/run-daemon wayland-launch ${SNAP}/qmlscene/qmlscene ${SNAP}/app.qml "$@"
+exec $SNAP/bin/run-daemon wayland-launch ${SNAP}/qmlscene/qmlscene ${SNAP}/gif.qml "$@"
 
